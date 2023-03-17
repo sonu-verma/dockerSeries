ac6b30d9b18e04de7c53843ca680595f2e6fa46f
sonu.verma <sonu@inhabitr.in>
2023-03-18 02:09:06 +0530

    added changes related to keep all services on same network

--- src/index.php
+++ src/index.php
@@ -1,2 +1,14 @@
 <?php
-   echo phpinfo();
+$servername = "db";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,'laraapp_db');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
